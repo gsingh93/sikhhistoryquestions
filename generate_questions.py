3c86303529a1f84d2ce8e48c969deb48c6d50cfd
Gulshan Singh <gsingh2011@gmail.com>
2015-09-26 20:56:02 -0700

    Updated script to handle questions with no answers and skip false positives

--- generate_questions.py
+++ generate_questions.py
@@ -24,7 +24,7 @@ import re
 import sys
 
 
-QUESTION_RE = re.compile('\d+\.\s(.*)')
+QUESTION_RE = re.compile('(\d+)\.\s(.*)')
 questions = []
 
 
@@ -42,17 +42,30 @@ def main():
         print 'Usage: %s input_file output_file' % sys.argv[0]
         exit(1)
 
+    last_num = 0
     with open(sys.argv[1]) as f:
         question = ''
         answer = ''
         for l in f.read().split('\n'):
             m = QUESTION_RE.match(l)
             if m is not None:
+                cur_num = int(m.group(1))
+                # Make sure the numbers are consecutive
+                if cur_num != last_num + 1:
+                    # Add line as an answer
+                    l = l.strip()
+                    if l != '':
+                        answer += l + '\n'
+                    continue
+                last_num = cur_num
+
                 if answer != '':
                     add_question(question, answer)
                     answer = ''
+                elif cur_num != 1: #TODO
+                    print 'Question %d missing answer' % (cur_num - 1)
 
-                question = m.group(1).strip()
+                question = m.group(2).strip()
             else:
                 l = l.strip()
                 if l != '':
@@ -63,6 +76,8 @@ def main():
 
     with open(sys.argv[2], 'w+') as f:
         json.dump(questions, f)
+
+    print 'Wrote %d questions to %s' % (len(questions), sys.argv[2])
 
 
 if __name__ == '__main__':
